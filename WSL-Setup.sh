752967f5460a1175326d0b0bee1b5e6c87abdc1d
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2022-03-20 18:23:42 +0100

    Icons

---
+++ WSL-Setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update
+sudo apt-get -y update; sudo apt-get upgrade -y
+
+# Install dependencies
+sudo apt-get install wget -y
+sudo apt-get install git -y
+code;
+
+# Install zsh and oh-my-zsh
+sudo apt-get install zsh -y
+sh -c "$(wget -4 https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y # Force IPv4 address
+
+# Install Powerline fonts
+sudo apt-get install fonts-powerline
+
+# Change theme
+echo "change with 'nano ~/.zshrc' ZSH_THEME=agnoster"
