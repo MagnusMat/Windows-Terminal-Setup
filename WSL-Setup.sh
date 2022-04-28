1be251c4cad9b684c59b9a71117fc60311186e40
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2022-04-28 15:09:21 +0200

    Update WSL-Setup.sh

--- WSL-Setup.sh
+++ WSL-Setup.sh
@@ -3,22 +3,22 @@
 cd ~
 
 # Update
-sudo apt-get -y update; sudo apt-get upgrade -y
+sudo apt -y update; sudo apt -y upgrade
 
 # Install dependencies
-sudo apt-get install wget -y
-sudo apt-get install git -y
+sudo apt-get -y install wget
+sudo apt-get -y install git
+sudo apt-get -y install zip
+sudo apt-get -y install unzip
 code;
-sudo apt-get install zip
-sudo apt-get install unzip
 
 # Install zsh and oh-my-zsh
-sudo apt-get install zsh -y
+sudo apt-get -y install zsh
 sh -c "$(wget -4 https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y # Force IPv4 address
 
 # Install Fonts
-sudo apt-get install fonts-powerline
-sudo apt-get install fonts-firacode
+sudo apt-get -y install fonts-powerline
+sudo apt-get -y install fonts-firacode
 
 # Change theme
 echo "change with 'nano ~/.zshrc' ZSH_THEME=agnoster"
