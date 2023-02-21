3c1d3218fab7071e16e30abe483781234177fd2b
Magnus Mathiesen <magnus_mat@pm.me>
2023-02-21 22:14:04 +0100

    Added Clang instead of g++

--- WSL-Setup.sh
+++ WSL-Setup.sh
@@ -9,12 +9,11 @@ sudo apt -y update; sudo apt -y upgrade
 sudo apt-get -y install wget
 sudo apt-get -y install git
 sudo apt-get -y install build-essential
-sudo apt-get -y install g++
+sudo apt-get -y install clang
 sudo apt-get -y install zip
-sudo apt-get -y install unzip
 sudo apt-get -y install curl
 sudo apt-get -y install apt-transport-https
-code;
+code
 
 # GitHub CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -35,4 +34,3 @@ sh -c "$(wget -4 https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/inst
 
 # Change Zsh Theme
 wget -O ~/.zshrc https://raw.githubusercontent.com/MagnusMat/Windows-Terminal-Setup/main/.zshrc
-
