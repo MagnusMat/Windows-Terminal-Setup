7e15fd9fa6fa207d7000c677a42d707a067b90f4
Magnus Mathiesen <magnus_mat@pm.me>
2023-10-13 01:12:22 +0200

    Update WSL-Setup.sh

--- WSL-Setup.sh
+++ WSL-Setup.sh
@@ -6,13 +6,13 @@ cd ~
 sudo apt -y update; sudo apt -y upgrade
 
 # Install dependencies
-sudo apt-get -y install wget
-sudo apt-get -y install git
-sudo apt-get -y install build-essential
-sudo apt-get -y install clang
-sudo apt-get -y install zip
-sudo apt-get -y install curl
-sudo apt-get -y install apt-transport-https
+sudo apt -y install wget
+sudo apt -y install git
+sudo apt -y install build-essential
+sudo apt -y install clang
+sudo apt -y install zip
+sudo apt -y install curl
+sudo apt -y install apt-transport-https
 
 # GitHub CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -24,11 +24,11 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 gh auth login
 
 # Install Fonts
-sudo apt-get -y install fonts-powerline
-sudo apt-get -y install fonts-firacode
+sudo apt -y install fonts-powerline
+sudo apt -y install fonts-firacode
 
 # Install zsh and oh-my-zsh
-sudo apt-get -y install zsh
+sudo apt -y install zsh
 sh -c "$(wget -4 https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y # Force IPv4 address
 
 # Change Zsh Theme
